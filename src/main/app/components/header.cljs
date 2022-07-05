15022708f232866d6eeea68070abd09d543ecbf5
Felipe Horizonte <felipehorizonte@santriweb.com.br>
2022-07-04 22:05:34 -0300

    css header

--- src/main/app/components/header.cljs
+++ src/main/app/components/header.cljs
@@ -4,8 +4,8 @@
         [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
         [com.fulcrologic.fulcro.routing.dynamic-routing :as dr :refer [defrouter]]))
 
-(defn HeaderComponent [this props]
-    (dom/div {:style {:background-color "red" :width "100%" :height "75px"}}
+(defn HeaderComponent [this]
+    (dom/div {:id "header"}
           (dom/button {:onClick #(dr/change-route this ["homepage"])} "Pagina principal")
           (dom/button {:onClick #(dr/change-route this ["loginpage"])} "Pagina de login")))
 
